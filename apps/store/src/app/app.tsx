34565eb4c1d3c06a54aa97b2e7f20f89fdbf2baa
Gary Huang <garyhuaang@gmail.com>
2025-04-23 14:44:53 -0500

    inital setup done, shadcnui install wip

--- apps/store/src/app/app.tsx
+++ apps/store/src/app/app.tsx
@@ -1,6 +1,7 @@
 import NxWelcome from './nx-welcome';
 
 import { Route, Routes, Link } from 'react-router-dom';
+import { Layout } from '@sas-mrts/layout';
 
 export function App() {
   return (
@@ -16,31 +17,12 @@ export function App() {
       <div role="navigation">
         <ul>
           <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/page-2">Page 2</Link>
+            <Link to="/layout">Layout</Link>
           </li>
         </ul>
       </div>
       <Routes>
-        <Route
-          path="/"
-          element={
-            <div>
-              This is the generated root route.{' '}
-              <Link to="/page-2">Click here for page 2.</Link>
-            </div>
-          }
-        />
-        <Route
-          path="/page-2"
-          element={
-            <div>
-              <Link to="/">Click here to go back to root page.</Link>
-            </div>
-          }
-        />
+        <Route path="/layout" element={<Layout />} />
       </Routes>
       {/* END: routes */}
     </div>
---
+++ libs/pages/layout/src/lib/layout.tsx
@@ -0,0 +1,9 @@
+export function Layout() {
+  return (
+    <div>
+      <h1>Welcome to Layout!</h1>
+    </div>
+  );
+}
+
+export default Layout;
