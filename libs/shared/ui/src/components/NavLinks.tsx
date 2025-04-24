9fda5e6a478b46ffa97449469b4b0368471b6cc3
Gary Huang <garyhuaang@gmail.com>
2025-04-24 18:24:34 -0500

    SHADCN WOPRKING JESUS

--- libs/shared/ui/src/components/NavLinks.tsx
+++ libs/shared/ui/src/components/NavLinks.tsx
@@ -1,13 +1,13 @@
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'
 
-import { Button } from '../base/Button';
+import { Button } from '../base/Button'
 import {
   NavigationMenu,
   NavigationMenuItem,
   NavigationMenuList,
-} from '../base/NavigationMenu';
+} from '../base/NavigationMenu'
 
-import { Route } from '@sas-mrts/common';
+import { Route } from '@sas-mrts/common'
 
 function NavLinks({ routes }: { routes: Route[] }) {
   return (
@@ -15,14 +15,14 @@ function NavLinks({ routes }: { routes: Route[] }) {
       <NavigationMenuList>
         {routes.map((route) => (
           <NavigationMenuItem key={route.name}>
-            <Button asChild>
+            <Button asChild variant="outline">
               <Link to={route.path}>{route.name}</Link>
             </Button>
           </NavigationMenuItem>
         ))}
       </NavigationMenuList>
     </NavigationMenu>
-  );
+  )
 }
 
-export { NavLinks };
+export { NavLinks }
