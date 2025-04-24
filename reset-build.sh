9dc9924b85772d63bd31439c29caf8682ce43cf8
Gary Huang <garyhuaang@gmail.com>
2025-04-24 16:26:30 -0500

    npm -> pnpm as pack mngr & resolved router type error

--- reset-build.sh
+++ reset-build.sh
@@ -4,11 +4,12 @@
 echo "🧹 Cleaning Nx cache..."
 nx reset
 nx clear-cache
-rm -rf node_modules/.vite
 rm -rf ./node_modules/.cache/nx
-rm -rf ./dist
-rm -rf ./.nx
+rm -rf .//dist
+rm -rf .//.nx
 rm -rf dist/out-tsc
+rm -rf ./node_modules
+rm -f ./pnpm-lock.yaml
 
 echo "📦 Installing dependencies..."
 npm i
