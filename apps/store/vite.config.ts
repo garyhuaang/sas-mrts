bf3824a77de00759ceaa3fb6a98aaa41f9870111
Gary Huang <garyhuaang@gmail.com>
2025-05-18 00:15:04 -0500

    SAS-16: workflows added

--- apps/store/vite.config.ts
+++ apps/store/vite.config.ts
@@ -6,6 +6,7 @@ import svgr from 'vite-plugin-svgr'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig(() => ({
+  base: '/sas-mrts/',
   root: __dirname,
   cacheDir: '../../node_modules/.vite/apps/store',
   envDir: path.resolve(__dirname, 'stripe-server'),
