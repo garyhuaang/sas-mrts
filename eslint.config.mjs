80a04406f88c73eee9019ae193cc02b156d896f1
Gary Huang <garyhuaang@gmail.com>
2025-05-18 09:42:04 -0500

    fixed chunk size issue

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,6 +19,7 @@ export default [
       '**/vitest.config.*.timestamp*',
       '**/node_modules',
       '**/libs/shared/ui/src/base/*',
+      '**/libs/shared/ui/src/lib/*',
       '**/libs/rStore/src/lib/api/*',
       '**/apps/store/stripe-server/*',
     ],
