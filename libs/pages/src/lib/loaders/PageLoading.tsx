e53c406960a27e4b729871b09014ead688a4bb45
Gary Huang <garyhuaang@gmail.com>
2025-05-28 18:08:57 -0500

    pageloader on navigating to other page.... WIP

---
+++ libs/pages/src/lib/loaders/PageLoading.tsx
@@ -0,0 +1,39 @@
+import gsap from 'gsap'
+import { useRef } from 'react'
+
+import './styles.css'
+
+import { useGSAP } from '@gsap/react'
+
+function PageLoading({ loading }: { loading: boolean }) {
+  gsap.registerPlugin(useGSAP)
+
+  const spinner = useRef(null)
+
+  useGSAP(() => {
+    if (!loading) {
+      gsap.to(spinner.current, {
+        opacity: 0,
+        duration: 0.5,
+        zIndex: -100,
+      })
+    }
+  }, [loading])
+
+  return (
+    loading && (
+      <div className="spinner-wrapper bg-background" ref={spinner}>
+        <div className="spinner">
+          <div className="sk-folding-cube">
+            <div className="sk-cube1 sk-cube" />
+            <div className="sk-cube2 sk-cube" />
+            <div className="sk-cube4 sk-cube" />
+            <div className="sk-cube3 sk-cube" />
+          </div>
+        </div>
+      </div>
+    )
+  )
+}
+
+export { PageLoading }
--- libs/shared/ui/src/composite/navigation/NavBar.tsx
+++ libs/shared/ui/src/composite/navigation/NavBar.tsx
@@ -4,21 +4,22 @@ import { NavLogo } from './NavLogo'
 
 import { Route } from '@sas-mrts/rStore'
 
-function NavBar({
-  routes,
-  className,
-}: {
+type NavBarProps = {
   routes: Route[]
   className?: string
-}) {
+  onLinkClick: () => void
+  onCartClick: () => void
+}
+
+function NavBar({ routes, className, onLinkClick, onCartClick }: NavBarProps) {
   return (
     <div
       className={`flex h-20 py-4 px-8 bg-muted justify-between items-center
-        shadow-md ${className}`}
+         ${className}`}
     >
       <NavLogo />
-      <NavLinks routes={routes} />
-      <NavActions />
+      <NavLinks routes={routes} onLinkClick={onLinkClick} />
+      <NavActions onCartClick={onCartClick} />
     </div>
   )
 }
