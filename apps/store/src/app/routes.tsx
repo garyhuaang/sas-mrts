cc430da25d2c458f298370bbb1e62bec25ea4c4a
Gary Huang <garyhuaang@gmail.com>
2025-06-03 12:43:40 -0500

    Reacth Hashrouter to avoid 404 error on refresh

--- apps/store/src/app/routes.tsx
+++ apps/store/src/app/routes.tsx
@@ -1,27 +1,22 @@
-import { createBrowserRouter, type RouteObject } from 'react-router-dom'
+import { createHashRouter, type RouteObject } from 'react-router-dom'
 
 import { Auth, Checkout, Layout, Return, userShopRoutes } from '@sas-mrts/pages'
 
-const router = createBrowserRouter(
-  [
-    {
-      path: '/',
-      element: <Layout />,
-      children: userShopRoutes.map(
-        (route): RouteObject => ({
-          index: route.path === '/',
-          path: route.path === '/' ? undefined : route.path,
-          element: route.element,
-        })
-      ),
-    },
-    { path: '/checkout', element: <Checkout /> },
-    { path: '/return', element: <Return /> },
-    { path: '/auth', element: <Auth /> },
-  ],
+const router = createHashRouter([
   {
-    basename: '/sas-mrts/',
-  }
-)
+    path: '/',
+    element: <Layout />,
+    children: userShopRoutes.map(
+      (route): RouteObject => ({
+        index: route.path === '/',
+        path: route.path === '/' ? undefined : route.path,
+        element: route.element,
+      })
+    ),
+  },
+  { path: '/checkout', element: <Checkout /> },
+  { path: '/return', element: <Return /> },
+  { path: '/auth', element: <Auth /> },
+])
 
 export default router
